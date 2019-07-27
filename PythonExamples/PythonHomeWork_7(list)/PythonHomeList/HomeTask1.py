3e6d4c3b25349b488d270531bcb958680e47c46f
duk1edev <duk1e.ptc.ua@yandex.ru>
2019-07-27 13:46:15 +0800

    added explanation string for all functions

--- PythonExamples/PythonHomeWork_7(list)/PythonHomeList/HomeTask1.py
+++ PythonExamples/PythonHomeWork_7(list)/PythonHomeList/HomeTask1.py
@@ -1,16 +1,34 @@
 def min_max(my_list):
-    print("min = {}".format(min(my_list)))
-    print("max = {}".format(max(my_list)))
+    """Searchin min and max value of list
+    """
+    print("Min = ",min(my_list,key = abs))
+    print("Max = ",max(my_list,key = abs))
+
+def sum_all_element(my_list):
+    """Get the all elements of list 
+    and return sum of all elements of list
+    """
+    result = 0
+    for i in range(len(my_list)):
+        result = result + int(my_list[i])
+    return result
+
+def middle_arif(my_list):
+    """Находит среднее ариметичское значение 
+    их всех сначений списка
+    """
+    result = sum_all_element(my_list) / len(my_list)
+    return result
 
 my_list = []
 
 size = int(input("Enter the size of list: "))
 
-for x in range(0,size,1):
-     my_list.append(input("index[{}] = ".format(x)))
+for x in range(size):
+     my_list.append(int(input("index[{}] = ".format(x))))
 
 print("My_List: {}",format(my_list))  
 
 min_max(my_list)
-
-
+print("Sum of all list elements : {}".format(sum_all_element(my_list)))
+print("Middle arif : {}".format(middle_arif(my_list)))
