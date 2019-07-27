900943616f7d7443493adc6bb9262bc75ae17432
duk1edev <duk1e.ptc.ua@yandex.ru>
2019-07-27 14:59:21 +0800

    bad algotothm

---
+++ PythonExamples/PythonHomeWork_7(list)/PythonHomeList/HomeTask3.py
@@ -0,0 +1,11 @@
+#create simple number
+
+simple_number = []
+
+for i in range(2,100):
+    for j in range(2,i):
+        if i % j == 0:
+            break
+        else:
+            simple_number.append(i)
+print(simple_number)
