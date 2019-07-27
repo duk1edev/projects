8ebd23c26cc994ae6a88ccfce9c6837848861ca4
duk1edev <duk1e.ptc.ua@yandex.ru>
2019-07-27 22:52:22 +0800

    Check of simple number or not(if i % j(2,i) == 0, return key ==false , brake; if key == True: thi number is simple

--- PythonExamples/PythonHomeWork_7(list)/PythonHomeList/HomeTask3.py
+++ PythonExamples/PythonHomeWork_7(list)/PythonHomeList/HomeTask3.py
@@ -1,11 +1,14 @@
 #create simple number
+N = int(input("Enter the lenth of number list: "))
 
 simple_number = []
 
-for i in range(2,100):
+for i in range(2,N+1):
+    flag = True
     for j in range(2,i):
         if i % j == 0:
+            flag = False
             break
-        else:
+    if flag:
             simple_number.append(i)
 print(simple_number)
